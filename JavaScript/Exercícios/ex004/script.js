38e17b32ae35c6a7fab7ae8561badbb761f8df2a
William Colasso <william.colasso@gmail.com>
2024-05-28 20:45:50 -0300

    .

---
+++ JavaScript/Exercícios/ex004/script.js
@@ -0,0 +1,5 @@
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName('p')[0]
+window.document.write('Esteve escrito assim:'+p1.innerText)
+p1.style.color = 'red'
+corpo.style.background = 'black'
