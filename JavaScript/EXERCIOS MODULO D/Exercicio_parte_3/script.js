bd3ac8f99068f8fbc54a4d002e5146a201c203fa
William Colasso <william.colasso@gmail.com>
2024-05-31 16:28:39 -0300

    Finalização do exercício

--- JavaScript/EXERCIOS MODULO D/Exercicio_parte_3/script.js
+++ JavaScript/EXERCIOS MODULO D/Exercicio_parte_3/script.js
@@ -1,25 +1,71 @@
-var ano = document.getElementById('nasceu')
-ano = Number(ano.value)
-var foto = document.getElementById('foto')
-var mullher
-var homem
-var idade = 2024-ano
 
 function calcular(){
-    alert(ano)
-    if(homem){
-        if(ano<18){
-            foto.src = 'adolescente_homem.png'
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var idade = ano - Number(fano.value)
+    var img = document.createElement('img')
+    var fsex = document.getElementsByName('genero')
+    var genero = ''
+    var resu = document.getElementById('res')
+    img.setAttribute('id', 'foto')
+
+
+    if(fsex[0].checked){
+        genero = ' homem'
+        if(idade<0){
+            //Esperma
+            img.setAttribute('src', 'esperma.png')
         }
+        else if(idade<12){
+            //Moleque
+            img.setAttribute('src', 'moleque.png')
+        }
+        else if(idade<18){
+            //Adolescente pia
+            img.setAttribute('src', 'adolescente_homem.png')
+        }
+        else if(idade<60){
+            //Adulto
+            img.setAttribute('src', 'homem.png')
+        }
+        else if(idade<100){
+            //Idoso
+            img.setAttribute('src', 'velho.png')
+        }
+        else{
+            //Esqueleto
+            img.setAttribute('src', 'esqueleto.png')
+        }
+    }else{
+        genero='a mulher'
+        if(idade<0){
+            //Esperma
+            img.setAttribute('src', 'esperma.png')
+        }
+        else if(idade<12){
+            //Moleca
+            img.setAttribute('src', 'garota.png')
+        }
+        else if(idade<18){
+            //Adolescente guria
+            img.setAttribute('src', 'adolescente_mulher.png')
+        }
+        else if(idade<60){
+            //Adulta
+            img.setAttribute('src', 'mulher.png')
+        }
+        else if(idade<100){
+            //Idosa
+            img.setAttribute('src', 'velha.png')
+        }
+        else{
+            //Esqueleto
+            img.setAttribute('src', 'esqueleto.png')
+        }
+
     }
-}
-
-function decidir(){
-     mullher = true
-     homem = false
-}
-
-function decidir2(){
-    mullher = false
-    homem = true
+    
+    resu.innerHTML = `Detectamos um${genero} com ${idade} anos de idade`
+    resu.appendChild(img)
 }
