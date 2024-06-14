6330ad8715540a911d70050cdbd1990958e37c68
William Colasso <william.colasso@gmail.com>
2024-06-14 11:42:58 -0300

    novo exercicio

--- JavaScript/Exercícios/ex011/ambiente.js
+++ JavaScript/Exercícios/ex011/ambiente.js
@@ -1,4 +1,34 @@
-for( i=0; i<10;i++){
+var metodo = 1
+c = 0
+//Inicializando Variáveis
 
-    console.log("Hello "+(i+1))
+if (metodo === 1) {
+    console.log("Este é o metodo WHILE")
+                                                //Laço de repetição WHILE
+    while (c <= 10) {
+        console.log(`Hello ${c}`)
+        c++
+    }
+
+
+
+}
+
+else if (metodo === 2) {
+    console.log("Este é o metodo DO WHILE")
+    do {
+        console.log(`Hello ${c + 1}`)       //Laço de repetição DO WHILE
+        c++
+    }
+    while (c <= 10)
+}
+
+else {
+    console.log("Este é o metodo FOR")
+    for (i = 0; i < 10; i++) {
+                                            //Laço de repetição FOR
+
+        console.log("Hello " + (i + 1))
+    }
+
 }
